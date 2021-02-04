be64e9c939e1e091592a54ec9429c3628812b6c6
Jeffrey <JWohlfield@hotmail.com>
2021-02-04 16:26:52 -0500

    More Basket updates

--- Group3_MidtermKrustyKrab/Basket.cs
+++ Group3_MidtermKrustyKrab/Basket.cs
@@ -6,5 +6,25 @@ namespace Group3_MidtermKrustyKrab
 {
     class Basket
     {
+        public Product Item { get; set; }
+        
+        public int Quantity { get; set; }
+
+        public Basket (Product Item, int Quantity)
+        {
+            this.Item = Item;
+            this.Quantity = Quantity;
+        }
+
+        public void DisplayBasket()
+        {
+
+        }
+
+        public void RemoveItem()
+        {
+
+        }
+
     }
 }
