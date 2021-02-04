88dda47040ebe94376a642a136b2d0fdd4d1c39d
thebestjuliana <76972376+thebestjuliana@users.noreply.github.com>
2021-02-04 17:43:32 -0500

    Update

--- Group3_MidtermKrustyKrab/MainMenu.cs
+++ Group3_MidtermKrustyKrab/MainMenu.cs
@@ -6,5 +6,24 @@ namespace Group3_MidtermKrustyKrab
 {
     class MainMenu
     {
+        List<string> Options = new List<string>();
+        string option1 = "See item description";
+        string option2 = "Add product to Cart";
+        string option3 = "Remove item from Cart";
+       //string option4 = "View Cart";
+        //string option5 = "Empty Cart";
+        string option6 = "Back to previous menu";
+        //string optioin7 = "emplyee mode";
+
+    }
+
+    public static void PrintOptions(List<string> Options)
+    {
+        int i = 1;
+        foreach (string option in Options)
+        {
+            Console.WriteLine($" {i}. {option}");
+            i++;
+        }
     }
 }
