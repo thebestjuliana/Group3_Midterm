fe7d0209956ad4d6cd750314aab9b434aefca282
thebestjuliana <76972376+thebestjuliana@users.noreply.github.com>
2021-02-04 17:13:29 -0500

    Big push at the end of the day

---
+++ Group3_MidtermKrustyKrab/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group3_MidtermKrustyKrab
+{
+    class Inventory
+    {
+        public List<Product> Catalogue { get; set; }
+
+        public Inventory()
+        {
+           
+            List<Product> Catalogue = new List<Product>();
+
+            Product a = new Product("Italian Sub", ProductCategory.Sandwich, "Meatballs on a hoagie", 7.00);
+            Product b = new Product("Chocolate Chip Cookie", ProductCategory.Dessert, "Made with love from your granny", 5.00);
+
+            Catalogue.Add(new Product("Sugar Cookie", ProductCategory.Dessert, "Made with sugar", 5.00));
+
+            Catalogue.Add(a);
+            Catalogue.Add(b);
+        }
+    }
+}
