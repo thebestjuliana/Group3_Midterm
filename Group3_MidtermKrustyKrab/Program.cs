855a40d8a4c42c0f9d21b69b99bc1db9f7d94aac
thebestjuliana <76972376+thebestjuliana@users.noreply.github.com>
2021-02-04 13:44:19 -0500

    initial commit

---
+++ Group3_MidtermKrustyKrab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Group3_MidtermKrustyKrab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
