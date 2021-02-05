f182f81be653e9a032f933bb30df2eaeab414391
Jeffrey <JWohlfield@hotmail.com>
2021-02-05 10:03:45 -0500

    Updated Basket Class

--- Group3_MidtermKrustyKrab/Basket.cs
+++ Group3_MidtermKrustyKrab/Basket.cs
@@ -6,8 +6,9 @@ namespace Group3_MidtermKrustyKrab
 {
     class Basket
     {
+        double salesTax = .06;
         public Product Item { get; set; }
-        
+        public string ProductName { get; set; }
         public int Quantity { get; set; }
 
         public Basket (Product Item, int Quantity)
@@ -16,14 +17,28 @@ namespace Group3_MidtermKrustyKrab
             this.Quantity = Quantity;
         }
 
-        public void DisplayBasket()
+        public void UpdateQuantity(int num)
         {
-
+            Quantity = num;
+        }
+        public double TotalItem()
+        {
+            return Quantity * Item.Price * (1 + salesTax);
+        }
+        public string TotalFormatedItem()
+        {
+            return FormatNumber(TotalItem());
         }
 
-        public void RemoveItem()
+        private string FormatNumber(double x)
         {
+            return x.ToString("C2");
+        }
 
+        public override string ToString()
+        {
+            string output = $"{ProductName,-20}{Quantity,-5}{TotalFormatedItem(),-5}";
+            return output;
         }
 
     }
